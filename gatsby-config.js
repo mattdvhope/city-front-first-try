0389713ddef2bf78ccaa205593fbe255357926b1
mattdvhope <mattagape@me.com>
2018-05-10 09:24:54 -0400

    Added 'gatsby-plugin-typography'

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-typography',
     {
       resolve: 'gatsby-transformer-remark',
       options: {

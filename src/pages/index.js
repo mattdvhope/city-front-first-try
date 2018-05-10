f7daa8b8fb70b1b2803789091c7f8ae29550d64d
mattdvhope <mattagape@me.com>
2018-05-10 14:01:08 -0400

    Edited text

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,28 @@ export default class FrontPage extends React.Component {
             <h1 className="has-text-weight-bold is-size-2">Latestttt Stories</h1>
           </div>
 
-          <h1>Hello from FRONT PAGE</h1>
+          <div>
+            <h1>Richard Hamming on Luck</h1>
+            <div>
+              <p>
+                From Richard Hamming’s classic and must-read talk, “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+                  You and Your Research
+                </a>”.
+              </p>
+              <blockquote>
+                <p>
+                  There is indeed an element of luck, and no, there isn’t. The prepared
+                  mind sooner or later finds something important and does it. So yes, it
+                  is luck.{" "}
+                  <em>
+                    The particular thing you do is luck, but that you do something is
+                    not.
+                  </em>
+                </p>
+              </blockquote>
+            </div>
+            <p>Posted April 09, 2011</p>
+          </div>
 
           <h3><Link to="blog-index">Go to blog index</Link></h3>
         </div>

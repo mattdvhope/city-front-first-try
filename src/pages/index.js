bb065b3c07d01120ae687a28b54a2654d3b24149
mattdvhope <mattagape@me.com>
2018-05-10 09:24:32 -0400

    Added blog-index page and moved blog to that page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import axios from 'axios'
 
-export default class IndexPage extends React.Component {
+export default class FrontPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    // const { data } = this.props
+    // const { edges: posts } = data.allMarkdownRemark
 
     return (
       <section className="section">
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latesttttt Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">Latestttt Stories</h1>
           </div>
-          {posts
-            .filter(post => post.node.frontmatter.templateKey === 'blog-post')
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
+
+          <h1>Hello from FRONT PAGE</h1>
+
+          <h3><Link to="blog-index">Go to blog index</Link></h3>
         </div>
       </section>
     )
   }
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
 
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
+// axios.get(`https://cityenglishproject.com/class_times.json`)
+//   .then(function(response){
+//     console.log(response.data); // ex.: { user: 'Your User'}
+//     console.log(response.status); // ex.: 200
+//   });
+

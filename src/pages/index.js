9da20d982efe7e338878c9d58aa19bd423f78e88
mattdvhope <mattagape@me.com>
2018-05-15 16:32:48 -0400

    Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ axios.get(`https://enigmatic-castle-3874.herokuapp.com/class_times.json`)
 export default class FrontPage extends React.Component {
   render() {
     return (
-        <div>
+        <div className="container-full">
           <ControlledCarousel />
           <WelcomeCaption />
         </div>

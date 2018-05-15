f793c4fa073a5bfccdb57785f2d9d7931a5e34e0
mattdvhope <mattagape@me.com>
2018-05-15 16:16:31 -0400

    Set up sass background imaging

--- src/components/Welcome-caption.js
+++ src/components/Welcome-caption.js
@@ -12,7 +12,7 @@ export default class WelcomeCaption extends React.Component {
 <div className="container-full">
   <div className="row">
     <div className="welcome-screen">
-      <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Welcome+mobile.jpg" alt="mobile picture" className="img-responsive" />
+      <img src="https://s3-ap-southeast-1.amazonaws.com/ccmcoversbsc/Welcome+mobile.jpg" alt="wide picture" className="img-responsive" />
       <div className="welcome-caption-content">
         The City English Project makes learning English convenient, affordable and fun!  And, we focus on what you need the most - conversational English skills. Our network offers classes, conversation partnerships and events to help you improve your conversational English. We also integrate culture and worldview elements in to our learning model to better equip your for building relationships within the ASEAN community and beyond.  We want to help you succeed in life and in business! <span id="get-to-be-a-member">Click here</span> to learn more about us. Or, <a className="modal-initiator non-summer-university"  href="#" data-toggle="modal" data-target="#applicationmodal">click here</a> to sign up for our popular "You Can Speak!" English class.
       </div>

25bc006b90f7712d6c07cc6edb61f7b2e7f95478
mattdvhope <mattagape@me.com>
2018-05-22 10:53:08 -0400

    Updated node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,17 @@ const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === "build-html") {
+    config.loader(
+      "null",
+      { test: /mdbreact/,
+        loader: "null-loader"
+      });
+  }
+};
+
+
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
 

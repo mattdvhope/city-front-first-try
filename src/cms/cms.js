abfc2b8dca145aead924daabc38c87327e2cc71d
mattdvhope <mattagape@me.com>
2018-06-14 05:47:40 -0400

    Removed fabulous page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,9 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import LibraryPagePreview from './preview-templates/LibraryPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import FabulousPagePreview from './preview-templates/FabulousPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('library', LibraryPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('fabulous', FabulousPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)

971dab039bea2aba5b9e37fb33dc237d1de46b0b
mattdvhope <mattagape@me.com>
2018-06-14 08:39:44 -0400

    added meta tag for screen width

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,6 +14,7 @@ const TemplateWrapper = ({ children }) => (
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
       <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/278cd7f5-226e-4ad9-83fb-59e4a7eb4131.css" />
       <link href="https://fonts.googleapis.com/css?family=Athiti|Chonburi|Kanit|Maitree|Prompt|Sriracha|Taviraj|Trirong|Josefin+Sans" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Helmet>
   	<NavbarOnTop />
 

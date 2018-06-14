b8f83b1e4ef118c4869ee064644d2228dfb8557f
mattdvhope <mattagape@me.com>
2018-06-14 18:08:12 -0400

    Setting up cms for WelcomeCaption

--- src/cms/preview-templates/WelcomeCaptionPreview.js
+++ src/cms/preview-templates/WelcomeCaptionPreview.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import WelcomeCaption from '../../templates/home-page/WelcomeCaption';
+import React from 'react';
+import PropTypes from 'prop-types';
+import WelcomeCaption from '../../templates/home-page/WelcomeCaption';
 
-// const WelcomePagePreview = ({ entry, getAsset }) => {
+const WelcomePagePreview = ({ entry }) => {
 
-//   return (
-//     <WelcomeCaption
-//       description={entry.getIn(['data', 'description'])}
-//     />
-//   )
-// }
+  return (
+    <WelcomeCaption
+      description={entry.getIn(['data', 'description'])}
+    />
+  )
+}
 
-// WelcomePagePreview.propTypes = {
-//   entry: PropTypes.shape({
-//     getIn: PropTypes.func,
-//   }),
-//   getAsset: PropTypes.func,
-// }
+WelcomePagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
 
-// export default WelcomePagePreview
+export default WelcomePagePreview
